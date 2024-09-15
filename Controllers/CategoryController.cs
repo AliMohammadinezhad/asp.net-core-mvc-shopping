b50172dac80d4d7a9294863ecb5b71e4136bee2e
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-15 16:36:27 +0330

    Show Category Data in View

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Data;
+using MVCProject.Models;
 
 namespace MVCProject.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> categories = _context.Categories.ToList();
+            return View(categories);
         }
     }
 }
