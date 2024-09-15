a611c8cc93fa9e521c05c32162c783b564d86e58
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-15 14:43:14 +0330

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCProject.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
