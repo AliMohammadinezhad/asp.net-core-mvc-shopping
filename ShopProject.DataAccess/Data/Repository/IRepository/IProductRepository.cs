3792f4690ba467d36fdde1aef2a494981bcb2b08
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-17 18:59:03 +0330

    add product repository pattern

---
+++ ShopProject.DataAccess/Data/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using ShopProject.Models;
+
+namespace ShopProject.DataAccess.Data.Repository.IRepository;
+
+public interface IProductRepository : IRepository<Product>
+{
+    void Update(Product obj);
+}
---
+++ ShopProject.DataAccess/Data/Repository/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using ShopProject.DataAccess.Data.Repository.IRepository;
+using ShopProject.Models;
+
+namespace ShopProject.DataAccess.Data.Repository;
+
+public class ProductRepository : Repository<Product>, IProductRepository
+{
+    private readonly ApplicationDbContext _context;
+    public ProductRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public void Update(Product obj)
+    {
+        _context.Products.Update(obj);
+    }
+}
