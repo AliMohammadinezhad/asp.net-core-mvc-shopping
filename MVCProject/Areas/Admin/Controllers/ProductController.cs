b6ed848d1c4553b7ea95205547ea00ea6660c4b9
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-18 17:36:32 +0330

    acomplish CRUD operations on Product

---
+++ MVCProject/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopProject.DataAccess.Data.Repository.IRepository;
+using ShopProject.Models;
+
+namespace MVCProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Product> products = _unitOfWork.Product.GetAll().ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            Product product = _unitOfWork.Product.Get(p => p.Id == id);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(product);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            Product product = _unitOfWork.Product.Get(p => p.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+    }
+}
