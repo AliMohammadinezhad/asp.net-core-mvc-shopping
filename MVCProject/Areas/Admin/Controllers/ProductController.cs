ef0123e4bc9f274d9780e37117d0a795b1d16f6a
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-22 12:30:57 +0330

    Change Repository to Populate Category with include

--- MVCProject/Areas/Admin/Controllers/ProductController.cs
+++ MVCProject/Areas/Admin/Controllers/ProductController.cs
@@ -19,7 +19,7 @@ namespace MVCProject.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            List<Product> products = _unitOfWork.Product.GetAll().ToList();
+            List<Product> products = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             
             return View(products);
         }
--- ShopProject.DataAccess/Data/Repository/Repository.cs
+++ ShopProject.DataAccess/Data/Repository/Repository.cs
@@ -15,16 +15,30 @@ public class Repository<T> : IRepository<T> where T : class
         this.dbSet = _context.Set<T>();
     }
 
-    public IEnumerable<T> GetAll()
+    public IEnumerable<T> GetAll(string? includeProperties = null)
     {
         IQueryable<T> query = dbSet;
+        if (!string.IsNullOrEmpty(includeProperties))
+        {
+            foreach (string property in includeProperties.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(property);
+            }
+        }
         return query.ToList();
     }
 
-    public T Get(Expression<Func<T, bool>> filter)
+    public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
     {
         IQueryable<T> query = dbSet;
         query = query.Where(filter);
+        if (!string.IsNullOrEmpty(includeProperties))
+        {
+            foreach (string property in includeProperties.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(property);
+            }
+        }
         return query.FirstOrDefault();
     }
 
