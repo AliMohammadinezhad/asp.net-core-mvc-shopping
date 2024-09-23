751de2b8b88af10b31e2a937555d6f66c05eb549
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-23 18:21:04 +0330

    Change styles of pages

--- MVCProject/wwwroot/js/product.js
+++ MVCProject/wwwroot/js/product.js
@@ -1,6 +1,6 @@
 ﻿let dataTable;
 
-$(document).ready(function () {
+$(function () {
     loadDataTable();
 });
 
@@ -19,9 +19,10 @@ function loadDataTable() {
                 data: 'id',
                 "render": function (data) {
                     return `
-                    <div class="w-75 btn-group" role="group"></div>
+                    <div class="w-75 btn-group" role="group">
                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i>Edit</a>
                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i>Delete</a>
+                    </div>
                     `
                 },
                 "width": "25%"
