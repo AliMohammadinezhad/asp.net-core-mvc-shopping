2b7617be613ac77ad4b4b726af0e59c062773c54
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-23 19:02:32 +0330

    refactor application user name and change profile layout

--- MVCProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MVCProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -109,6 +109,14 @@ namespace MVCProject.Areas.Identity.Pages.Account
             public string? Role { get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
+
         }
 
 
@@ -142,6 +150,12 @@ namespace MVCProject.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.Name = Input.Name;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
