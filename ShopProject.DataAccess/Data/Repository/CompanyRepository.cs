88fed00711f28bdd59d927ef147872e0d227a0be
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-24 16:23:23 +0330

    add Company Repository and UnitOfWork

---
+++ ShopProject.DataAccess/Data/Repository/CompanyRepository.cs
@@ -0,0 +1,18 @@
+﻿using ShopProject.DataAccess.Data.Repository.IRepository;
+using ShopProject.Models;
+
+namespace ShopProject.DataAccess.Data.Repository;
+
+public class CompanyRepository : Repository<Company>, ICompanyRepository
+{
+    private readonly ApplicationDbContext _context;
+    public CompanyRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public void Update(Company obj)
+    {
+        _context.Update(obj);
+    }
+}
---
+++ ShopProject.DataAccess/Data/Repository/IRepository/ICompanyRepository.cs
@@ -0,0 +1,8 @@
+﻿using ShopProject.Models;
+
+namespace ShopProject.DataAccess.Data.Repository.IRepository;
+
+public interface ICompanyRepository : IRepository<Company>
+{
+    void Update(Company obj);
+}
