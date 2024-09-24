3bc93c2ae112bca5dc86096922b90a36be93c707
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-24 17:59:00 +0330

    add Company Controller and Views

---
+++ MVCProject/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopProject.DataAccess.Data.Repository.IRepository;
+using ShopProject.Models;
+using ShopProject.Models.ViewModels;
+using ShopProject.Utility;
+
+namespace MVCProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles = SD.Role_Admin)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Company> Companies = _unitOfWork.Company.GetAll().ToList();
+            
+            return View(Companies);
+        }
+
+
+        [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                //create
+                return View(new Company());
+            }
+            else
+            {
+                //update
+                Company companyObj = _unitOfWork.Company.Get(u => u.Id == id);
+                return View(companyObj);
+            }
+
+            
+
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Company obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(obj);
+                    TempData["success"] = "Company created successfully";
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(obj);
+                    TempData["success"] = "Company updated successfully";
+                }
+                
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+
+
+        }
+
+        #region API Calls
+
+        public IActionResult GetAll()
+        {
+            List<Company> companies = _unitOfWork.Company.GetAll().ToList();
+            return Json(new { data = companies });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var company = _unitOfWork.Company.Get(u => u.Id == id);
+            
+            if (company == null)
+                return Json(new { success = false, message = "error while deleting" });
+
+            _unitOfWork.Company.Remove(company);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Company Deleted Successfully"});
+        }
+
+        #endregion
+    }
+}
