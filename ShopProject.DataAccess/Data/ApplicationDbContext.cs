30ced4139ce6f1c0b74f377d38415c770ce03773
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-25 19:06:21 +0330

    add OrderHeader and OrderDetail to models

--- ShopProject.DataAccess/Data/ApplicationDbContext.cs
+++ ShopProject.DataAccess/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     public DbSet<Product> Products { get; set; }
     public DbSet<Company> Companies { get; set; }
     public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<OrderHeader> OrderHeaders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
