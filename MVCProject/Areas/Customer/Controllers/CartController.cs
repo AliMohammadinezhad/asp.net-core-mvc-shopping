d82cd17dfc9b39ad68e4897cb62781153b2a6655
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-25 15:37:46 +0330

    add Cart View and Controller

---
+++ MVCProject/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShopProject.DataAccess.Data.Repository.IRepository;
+using ShopProject.Models;
+using ShopProject.Models.ViewModels;
+using System.Security.Claims;
+
+namespace MVCProject.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    //[Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            string userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                    includeProperties: nameof(Product))
+            };
+            return View();
+        }
+    }
+}
