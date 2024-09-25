2d46fa5ae2e85fdf9c065ee7046167bc9c579a2b
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-25 15:36:57 +0330

    Create View Model for Cart Page

---
+++ ShopProject.Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,7 @@
+﻿namespace ShopProject.Models.ViewModels;
+
+public class ShoppingCartVM
+{
+    public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+    public double OrderTotal { get; set; }
+}
