8b5904e625be2a5a4b44861686e15494338a44f8
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-28 13:31:33 +0330

    add order View Model

---
+++ ShopProject.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,7 @@
+﻿namespace ShopProject.Models.ViewModels;
+
+public class OrderVM
+{
+    public OrderHeader OrderHeader { get; set; }
+    public IEnumerable<OrderDetail> OrderDetail { get; set; }
+}
