b92e56792615b479d10396d603823126cf673086
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-09-28 13:31:21 +0330

    add order controller, view and use Data Tables

---
+++ MVCProject/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using ShopProject.DataAccess.Data.Repository.IRepository;
+using ShopProject.Models;
+
+namespace MVCProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+
+        #endregion
+
+    }
+}
